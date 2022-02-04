5fb38209affa27c1b50b374a59284c499a01e87a
Ray Foss <ray@vblip.com>
2022-02-04 00:58:41 -0600

    Refactor for modern tastes

--- index.js
+++ index.js
@@ -6,21 +6,18 @@ const hexToBuf = (hex) => {
 }
 
 const b32ToHex = (base32) => {
-    var base32chars, bits, chunk, hex, i, val
-    base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
-    bits = ''
-    hex = ''
-    i = 0
-    while (i < base32.length) {
-        val = base32chars.indexOf(base32.charAt(i).toUpperCase())
+    const base32chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+    let bits = ''
+    let hex = ''
+
+    for (let i = 0; i < base32.length; i++) {
+        let val = base32chars.indexOf(base32.charAt(i).toUpperCase())
         bits += val.toString(2).padStart(5, '0')
-        i++
     }
-    i = 0
-    while (i + 4 <= bits.length) {
-        chunk = bits.substring(i, i + 4)
-        hex = hex + parseInt(chunk, 2).toString(16)
-        i += 4
+
+    for (let i = 0; i + 4 <= bits.length; i += 4) {
+        let chunk = bits.substring(i, i + 4)
+        hex += parseInt(chunk, 2).toString(16)
     }
     return hex
 }
